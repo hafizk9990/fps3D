1a0182d07e5f1744c7fcb6701507b691c06cc6af
Uzair Afzal <studies9990@gmail.com>
2023-04-06 21:03:55 +0500

    Local to world space, move left, right, up, down, jump

---
+++ Assets/Scripts/Helpers/TagHolder.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Axis : MonoBehaviour
+{
+  public const string HORIZONTAL_AXIS = "Horizontal";
+  public const string VERTICAL_AXIS = "Vertical";
+}
