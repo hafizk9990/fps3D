f2adbeeedeefcf7db2b84779092aef98a2e80a4c
Uzair Afzal <studies9990@gmail.com>
2023-04-24 18:29:03 +0500

    Attack points, radius, layers etc.

---
+++ Assets/Scripts/Character/AttackScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackScript : MonoBehaviour
+{
+  public float damage = 2f;
+  public float radius = 1f;
+  public LayerMask myLayerMask;
+
+  void Update()
+  {
+    Collider[] hits = Physics.OverlapSphere(transform.position, radius, myLayerMask);
+
+    if (hits.Length > 0 && hits[0].gameObject != null && hits[0].gameObject.activeSelf)
+    {
+      Debug.Log("We hit this thing: " + hits[0].gameObject.tag + Time.time);
+      gameObject.SetActive(false);
+    }
+  }
+}
